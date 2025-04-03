15c0a30e40ec56770f98a65ea01d8cf1cdec0269
naveenkumar756 <116255598+naveenkumar756@users.noreply.github.com>
2025-04-03 16:53:33 +0530

    updated

---
+++ javascript suresh techs/prompt/index.js
@@ -0,0 +1,10 @@
+// prompt
+let username=prompt("what is your name","ramm")
+console.log(username)
+console.log(`welcome ${username} to the class`)
+let height=prompt("please enter the height in (cms)")
+let weight=prompt("please enter the height in (kgs)")
+let heightinmeters=height/100
+console.log("heightinmeters",heightinmeters)
+console.log("height",height)
+console.log("weight",weight)
---
+++ javascript suresh techs/typeconversion/index.js
@@ -0,0 +1,67 @@
+let marks=100;
+let firstname="Naveen"
+let cash="1000";
+let emptystring="";
+let passed=true;
+let result;
+let myvalue=null;
+let bigvalue=400n;
+let s1=Symbol("id");
+
+console.log("marks",marks)
+console.log("firstname",firstname)
+console.log("cash",cash)
+console.log("emptystring",emptystring)
+console.log("passed",passed)
+console.log("result",result)
+console.log("myvalue",myvalue)
+console.log("bigvalue",bigvalue)
+console.log("s1",s1)
+console.log("=====String conversion=====");
+// string conversion
+let marksstrstr=String(100);
+let firstnamestr=String("Naveen")
+let cashstr=String(1000);
+let emptystringstr="";
+let passedstr=String(true);
+let resultstr;
+let myvaluestr=String(null);
+let bigvaluestr=String(400n);
+let s1str=String(Symbol("id"));
+
+console.log("marks",marksstrstr)
+console.log("firstname",firstnamestr)
+console.log("cash",cashstr)
+console.log("emptystring",emptystringstr)
+console.log("passed",passedstr)
+console.log("result",resultstr)
+console.log("myvalue",myvaluestr)
+console.log("bigvalue",bigvaluestr)
+console.log("s1",s1str)
+
+
+console.log("=====Numberconversion=====")
+// Number conversion
+
+
+let marksnum=Number(100);
+let firstnamenum=Number("Naveen")
+let cashnum=Number(1000);
+let emptystringnum="";
+let passednum=Number(true);
+let resultnum;
+let myvaluenum=Number(null);
+let bigvaluenum=Number(400n);
+// let s1num=Number(Symbol("id"));
+
+
+
+console.log("marks",marksnum)
+console.log("firstname",firstnamestr)
+console.log("cash",cashnum)
+console.log("emptystring",emptystringnum)
+console.log("passed",passednum)
+console.log("result",resultnum)
+console.log("myvalue",myvaluenum)
+console.log("bigvalue",bigvaluenum)
+// console.log("s1",s1num)
