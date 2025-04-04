4d59c70fd56fd7e4464876e02c73665c0c10418a
naveenkumar756 <116255598+naveenkumar756@users.noreply.github.com>
2025-04-04 16:54:11 +0530

    updated

---
+++ javascript suresh techs/Operators/index.js
@@ -0,0 +1,16 @@
+let num1=10;
+let num2=20;
+console.log("Addition",num1+num2)
+console.log("subtractio",num1-num2)
+console.log("multiplication",num1*num2)
+console.log("division",(num1/num2).toFixed(7))
+console.log("remainder",num1%num2)
+console.log("Exponentiation",num1**num2)
+console.log("sqaure",(num1+num2)**(1/2).toFixed(2))
+console.log("cube root",(num1+num2)**(1/3))
+console.log("fourth",(num1+num2)**(1/4))
+
+
+// tofixeddecimalpoints
+// let decimalpoints=num1/num2
+// console.log(decimalpoints.toFixed(5))
